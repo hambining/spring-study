4d0108eef962cc43145a819e75850616f0e76e69
hambining <hyunbini2942@gmail.com>
2024-12-06 20:23:02 +0900

    first commit

---
+++ ham/src/main/java/com/example/ham/HamApplication.java
@@ -0,0 +1,13 @@
+package com.example.ham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamApplication.class, args);
+	}
+
+}
---
+++ ham/src/test/java/com/example/ham/HamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
