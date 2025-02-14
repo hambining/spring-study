3e9e2fd55d762bcd9b1f75758986b99577ab60ea
hambining <hyunbini2942@gmail.com>
2025-02-14 19:14:32 +0900

    Add @Getter @Setter annotations

--- ham/src/main/java/com/example/domain/Member.java
+++ ham/src/main/java/com/example/domain/Member.java
@@ -1,5 +1,9 @@
 package com.example.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Member {
 
     private Long id;
@@ -10,22 +14,6 @@ public class Member {
 
     public Member(Long id, String name) {
         this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 }
