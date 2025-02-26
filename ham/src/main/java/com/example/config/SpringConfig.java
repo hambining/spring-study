787f9d3e234d6337685d68e2de5b60bde6b7a6af
hambining <hyunbini2942@gmail.com>
2025-02-26 17:40:47 +0900

    Add DataSource Setting to Config

--- ham/src/main/java/com/example/config/SpringConfig.java
+++ ham/src/main/java/com/example/config/SpringConfig.java
@@ -4,16 +4,30 @@ import com.example.repository.JdbcTemplateMemberRepository;
 import com.example.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+//    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    private DataSource dataSource;
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/testdb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("gusqls96%");
+        return dataSource;
+    }
 
     @Bean
     MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource());
     }
 }
