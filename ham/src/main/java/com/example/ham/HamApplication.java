9a1ac6b05ff781f771f72cfb15d833690e4bc3ee
hambining <hyunbini2942@gmail.com>
2025-02-28 05:49:36 +0900

    Remove unnecessary test codes in Application file

--- ham/src/main/java/com/example/ham/HamApplication.java
+++ ham/src/main/java/com/example/ham/HamApplication.java
@@ -1,31 +1,13 @@
 package com.example.ham;
 
-import com.example.ham.config.SpringConfig;
-import com.example.ham.domain.Member;
-import com.example.ham.repository.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class HamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HamApplication.class, args);
-
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-		MemberRepository repository = context.getBean(MemberRepository.class);
-
-		Member member = new Member(1L, "Ham");
-		Member member1 = new Member();
-		member1.setName("Ham2");
-		member1.setId(2L);
-		repository.save(member);
-		repository.save(member1);
-
-		repository.findAll().forEach(System.out::println);
-		repository.findById(1L).stream().forEach(System.out::println);
-		repository.findByName("Ham").stream().forEach(System.out::println);
 	}
 
 }
