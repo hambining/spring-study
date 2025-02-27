1c27a3bf857beffc8d4d7b34f35071034ff3e317
hambining <hyunbini2942@gmail.com>
2025-02-28 06:11:10 +0900

    Add getMembersResponse method

--- ham/src/main/java/com/example/ham/controller/MemberController.java
+++ ham/src/main/java/com/example/ham/controller/MemberController.java
@@ -1,7 +1,12 @@
 package com.example.ham.controller;
 
+import com.example.ham.domain.Member;
 import com.example.ham.service.MemberService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/members")
@@ -10,5 +15,12 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Member>> getMembersResponse() {
+        List<Member> members = memberService.findMembers();
+        return ResponseEntity.ok()
+                .body(members);
     }
 }
