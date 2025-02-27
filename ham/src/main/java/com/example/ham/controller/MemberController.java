d82938778ce991b31ea03ce22d1b411f17c9970d
hambining <hyunbini2942@gmail.com>
2025-02-28 06:04:29 +0900

    Create MemberController.java

---
+++ ham/src/main/java/com/example/ham/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.example.ham.controller;
+
+import com.example.ham.service.MemberService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
